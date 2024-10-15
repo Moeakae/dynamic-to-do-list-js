add26521e9813feab7d003933229b0dee88dca05
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-10-15 19:45:55 +0200

    update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Setup Event Listener for Page Load
 document.addEventListener("DOMContentLoaded", (event) => {
     console.log("DOM fully loaded and persed");
 });
+// select DOM Elements
+const addButton = document.getElementById("addbutton");
