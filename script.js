30ba1e8dc5b68ade010c0aeae5343d46ed21bcb6
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-10-15 20:51:22 +0200

    update script.js

--- script.js
+++ script.js
@@ -8,7 +8,18 @@ const tasklist = document.getElementById('task-list');
 const taskInput = document.getAnimations('task-input');
 const li = document.createElement('li');
 li.textContent = tasktext;
+// This array will hold the addtask 
+let addtask = [];
 // Define the addTask Function
 function addTask() {
-    
+    const addTask = (title, dueDate, priority);
+         checked: false
+    return title + dueDate + priority;
+}
+// Get the value of the input and remove the the whitespace
+const text = input.value.trin();
+if (text !== '') {
+    AudioDestinationNode(text);
+    input.value = '';
+    input.focus();
 }
